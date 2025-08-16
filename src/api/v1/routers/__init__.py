bee9587231379374b49a6e2c1d371b5326dca25e
jpaullopes <jpaullopes27@gmail.com>
2025-08-16 12:29:11 -0300

    minor changes

--- src/api/v1/routers/__init__.py
+++ src/api/v1/routers/__init__.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 from .temperature import router as temperature_router
 from .websocket import router as websocket_router
+from .health import router as health_router
 
 api_v1_router = APIRouter(prefix="/api/v1")
 api_v1_router.include_router(temperature_router, tags=["temperature"])
+api_v1_router.include_router(health_router, tags=["health"])
